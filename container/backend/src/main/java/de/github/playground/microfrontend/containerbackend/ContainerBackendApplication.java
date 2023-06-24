98f29a04a770fbca19f503d281eafa0673cfc9ef
Omar Goatfryed Sood <goatfryed@gmail.com>
2023-06-24 15:08:32 +0200

    feat: initial container backend setup
    based on spring initializr

---
+++ container/backend/src/main/java/de/github/playground/microfrontend/containerbackend/ContainerBackendApplication.java
@@ -0,0 +1,13 @@
+package de.github.playground.microfrontend.containerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContainerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContainerBackendApplication.class, args);
+	}
+
+}
---
+++ container/backend/src/test/java/de/github/playground/microfrontend/containerbackend/ContainerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package de.github.playground.microfrontend.containerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContainerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
