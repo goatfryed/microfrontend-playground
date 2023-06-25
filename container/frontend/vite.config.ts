1b22244bbef63880d9f270716db92bd54418671c
Omar Goatfryed Sood <goatfryed@gmail.com>
2023-06-25 11:31:35 +0200

    chore: basic container frontend

--- container/frontend/vite.config.ts
+++ container/frontend/vite.config.ts
@@ -3,5 +3,14 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  server: {
+    port: 19901,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+      }
+    }
+  }
 });
